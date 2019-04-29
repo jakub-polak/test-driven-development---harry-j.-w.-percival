006c18b286b90be3d398c3e591bf2b3d9785f2ed
jpolak95 <jakubpolak95@gmail.com>
2019-04-29 16:51:28 +0200

    view refactor

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_elements_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
