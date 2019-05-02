bb5043ac1fb6b41b6bcb0ec40b0247d072a0e947
jpolak95 <jakubpolak95@gmail.com>
2019-05-02 22:45:41 +0200

    new view and tests

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+
+from django.shortcuts import redirect, render
 from lists.models import Item
 
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
-        Item.objects.create(text=new_item_text)
-        return redirect('/')
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/lists/the-only-list-in-the-world/')
 
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
